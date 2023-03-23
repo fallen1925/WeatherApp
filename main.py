b0c9ef98263aba2339c9c7c5af0832399bf1f594
fallen1925 <pachjsiciliano@gmail.com>
2023-03-23 11:43:53 -0300

    Eliminar comentarios

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def getWeather(canvas):
     localidad = json_data['name'] + '-' + json_data['sys']['country']
     timezone_api = api
     timezone_data = r.get(timezone_api).json()
-    timezone_offset = timezone_data['timezone']  # Obtener el offset de la zona horaria en segundos
+    timezone_offset = timezone_data['timezone']
     condition = json_data['weather'][0]['main']
     temp = int(json_data['main']['temp']-273.15)
     min_temp = int(json_data['main']['temp_min']-273.15)
@@ -24,10 +24,10 @@ def getWeather(canvas):
     wind = json_data['wind']['speed']
     sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise'] - 21600))
     sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset'] - 21600))
-    current_time_utc = time.gmtime()  # Obtener la hora UTC actual
+    current_time_utc = time.gmtime()
     current_time_local = time.localtime(
-    time.mktime(current_time_utc) + timezone_offset)  # Convertir a la hora local de la ciudad
-    current_time_formatted = time.strftime('%I:%M %p', current_time_local)  # Formatear la hora local
+    time.mktime(current_time_utc) + timezone_offset)
+    current_time_formatted = time.strftime('%I:%M %p', current_time_local)
 
     final_info = condition + "\n" + str(temp) + "°c"
     final_data = "\n" + "Localidad: " + str(localidad) + "\n" + "Minima: " + str(min_temp) + "°C" + "\n" + "Maxima: " + str(max_temp) + "°C" +"\n" + "Presion: " + str(pressure) + "\n" +"Humedad: " + str(humidity) + "\n" + "Hora: " + current_time_formatted
